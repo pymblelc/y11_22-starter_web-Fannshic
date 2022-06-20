7791b700ccb5323fc7462054e25fbf1a490b19a3
cindy fan <cindy@Cindy.local>
2022-06-20 15:03:25 +1000

    AHHHHHHHH WHYWHAYUHIFDAHU

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let div = document.getElementById("whatbtn")
 
 button.addEventListener(
     "click",
-    function(){
+    function () {
         alert("Hello World!");
         div.innerHTML = "button1"
         div.style.backgroundColor = "grey"
